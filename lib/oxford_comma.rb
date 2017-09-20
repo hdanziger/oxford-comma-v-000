fee827957ced342067f5ee0099839a1bda0abef9
Hannah Danziger <hdanziger8@gmail.com>
2017-09-20 17:38:07 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,6 @@
 def oxford_comma(array)
-  array.join
-end
-
-def oxford_comma(array)
+  if array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
   array[0..-2].join(", ") + ", and " + array[-1]
 end
